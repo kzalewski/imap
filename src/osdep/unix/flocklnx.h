d48d3277c2e9a981d08cbcf6c0543298bcdc8c1b
Ken Zalewski <zalewski.senate@gmail.com>
2022-11-27 23:06:46 -0500

    More updates for Linux.  Considering elimination of all platforms other than Linux, so the library will be fully optimized for the Linux platform.  For now, small steps will be taken to modernize the code.

---
+++ src/osdep/unix/flocklnx.h
@@ -0,0 +1,7 @@
+
+#ifndef FLOCK_LNX_H
+#define FLOCK_LNX_H
+
+int safe_flock(int fd, int op);
+
+#endif
---
+++ src/osdep/unix/utime_port.h
@@ -0,0 +1,9 @@
+
+#ifndef UTIME_PORT_H
+#define UTIME_PORT_H
+
+#include <time.h>
+
+int portable_utime(char *file, time_t timep[2]);
+
+#endif
